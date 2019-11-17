132c1da408c8d194c100c685dcf70b8960e6ebc8
Nelson Knight <nelson.knight92@gmail.com>
2019-11-17 16:03:56 -0700

    Added all files

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [
+	
+	{
+	    name: "Tony Stark",
+        photo: "https://i.pinimg.com/originals/6d/eb/ad/6debad045cffb2c0fb91e30b8b822b30.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+    ]
+
+	},
+	{
+		name: "Michael Scott",
+		photo: "https://i.redd.it/6tbbl1855kqz.jpg",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Obi-Wan Kenobi",
+        photo: "https://i.pinimg.com/originals/c6/ca/b9/c6cab909c85eeaa7b48a26fe1f9017d9.jpg",
+        scores: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
